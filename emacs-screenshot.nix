650305636ff2abe1fbe215293c177c20be83d4c2
Daniel Nagy <danielnagy@posteo.de>
2026-07-09 23:28:45 +0200

    Add emacs-screenshot.nix for generating dual-theme SVG screenshots
    Modeled after tokei.el/emacs-screenshot.nix.  Renders the
    *nixos-package htop* detail buffer via xvfb-run, modus-themes, and
    ImageMagick import.  Uses nixVersions.latest for full nix-instantiate
    metadata (store path, build inputs, maintainers) with NIX_STATE_DIR
    set for sandboxed builds.
    
    Outputs:
      pngEmacs                -- shadowed light theme PNG
      svgDualThemeEmacs        -- dual-theme SVG (light/dark via CSS media query)
      optimizeEmacsScreenshot  -- optipng-optimized PNG per theme

---
+++ emacs-screenshot.nix
@@ -0,0 +1,141 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib ? pkgs.lib,
+}:
+
+rec {
+
+  mkEmacsScreenshot =
+    {
+      # Emacs Lisp code to run before taking the screenshot
+      emacsCode,
+      # change this if you want another format
+      name ? "emacs-screenshot.png",
+      emacs ? pkgs.emacs,
+      light ? true,
+      ...
+    }:
+    let
+      nixosEl = pkgs.callPackage ./default.nix { };
+    in
+    pkgs.runCommandLocal name
+      {
+        NIX_PATH = "nixpkgs=${pkgs.path}";
+        NIX_STATE_DIR = "/build/nix-state";
+        nativeBuildInputs = [
+          (emacs.pkgs.withPackages (e: [
+            e.magit-section
+            e.modus-themes
+            nixosEl
+          ]))
+          pkgs.xvfb-run
+          pkgs.iosevka
+          pkgs.imagemagick
+          pkgs.nixVersions.latest
+        ];
+        emacsCodeFile = pkgs.writeText "emacscode.el" emacsCode;
+        screenshotScript = pkgs.writeText "script.el" ''
+          (run-at-time 10 nil (lambda () (kill-emacs 1)))   ; fallback killing
+          (load-theme 'modus-${if light then "operandi" else "vivendi"} t)
+          (menu-bar-mode -1)
+          (tool-bar-mode -1)
+          (toggle-scroll-bar -1)
+          (message nil)                            ; clear out echo area
+          (defun screenshot-this-frame-and-exit ()
+            (kill-emacs
+              (call-process "import" nil nil nil
+                            "-window" (frame-parameter (car (frame-list)) 'window-id) (getenv "out"))))
+          (run-at-time 2 nil #'screenshot-this-frame-and-exit)
+        '';
+      }
+      ''
+        mkdir -p "$NIX_STATE_DIR"
+        HOME=$PWD \
+          xvfb-run --server-args="-screen 0 1024x576x24" \
+            emacs --quick -f package-initialize --fullscreen \
+            -l modus-themes \
+            --font Iosevka\ 18 \
+            -l $screenshotScript \
+            -l $emacsCodeFile
+      '';
+
+  emacsNixosScreenshot =
+    {
+      light ? true,
+    }:
+    mkEmacsScreenshot {
+      inherit light;
+      emacsCode = ''
+        (require 'nix-mode)
+        (require 'nixos)
+        (run-at-time 1 nil (lambda ()
+                             (nixos-package "htop")
+                             (delete-other-windows)))
+      '';
+    };
+
+  imageShadow =
+    image:
+    pkgs.runCommandLocal image.name { inherit image; } ''
+      ${pkgs.imagemagick}/bin/magick "$image" \
+        -gravity Northwest \
+        -bordercolor black -border 1 \
+        -mosaic +repage \
+        \( +clone -background black -shadow "80x3+3+3" \) \
+        +swap \
+        -background none -mosaic +repage $out
+    '';
+
+  optimizePng =
+    image:
+    pkgs.runCommandLocal ("opt-" + image.name)
+      {
+        inherit image;
+        optlevel = 9;
+        nativeBuildInputs = [ pkgs.optipng ];
+      }
+      ''
+        optipng -strip all -o$optlevel "$image" -out $out
+      '';
+
+  svgDualThemeText = pkgs.writeText "myfile.svg" ''
+    <?xml version="1.0" encoding="utf-8"?>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+         viewBox="0 0 1024 576" xml:space="preserve">
+      <defs>
+        <style type="text/css">
+            image.light { display: inherit; }
+            image.dark { display: none; }
+            @media ( prefers-color-scheme:dark ) {
+                image.light { display: none; }
+                image.dark { display: inherit; }
+            }
+        </style>
+      </defs>
+      <image class="light" height="576" width="1024" href="data:image/png;base64,@lightThemeB64@" ></image>
+      <image class="dark" height="576" width="1024" href="data:image/png;base64,@darkThemeB64@" ></image>
+    </svg>
+  '';
+
+  svgDualTheme =
+    imgFunc:
+    pkgs.runCommandLocal "emacs-screenshot.svg" { } ''
+      lightThemeB64=$(base64 -w0 < ${imgFunc true})
+      darkThemeB64=$(base64 -w0 < ${imgFunc false})
+      substitute ${svgDualThemeText} $out \
+        --subst-var lightThemeB64 \
+        --subst-var darkThemeB64
+    '';
+
+  imageShadowEmacs =
+    light:
+    imageShadow (emacsNixosScreenshot {
+      inherit light;
+    });
+
+  optimizeEmacsScreenshot = light: optimizePng (imageShadowEmacs light);
+
+  svgDualThemeEmacs = svgDualTheme optimizeEmacsScreenshot;
+
+  pngEmacs = imageShadowEmacs true;
+}
